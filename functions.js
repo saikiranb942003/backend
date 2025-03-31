b4b3236df61fd274d9dde648f42249aa5703ac00
a_rihant <arijain@gammastack.com>
2025-03-31 15:30:21 +0530

    first commit

---
+++ functions.js
@@ -0,0 +1,67 @@
+// functions
+ /*function add() {
+    return 2 + 4 ;
+
+}
+
+console.log(add);
+
+
+function add() {
+    return 3 + 4 ;
+}
+
+console.log(add());
+
+
+function add(a,b) {
+    return a + b ;
+}
+
+console.log(add(5,5));
+
+
+
+// expression fuctions
+
+const sum = function (b,c) {
+    return b + c;
+}
+ 
+console.log(sum(3,3));
+
+
+//arrow functions
+
+const sum = () =>
+{
+    return 2 + 3;
+}
+console.log(sum());
+
+
+const sum = (a,b) =>
+{
+    console.log(a+b);
+}
+
+sum(2,2);*/
+
+//constructors
+function Alien(name,tech) 
+{
+    this.name = name;
+    this.tech = tech;
+    this.work = function ()
+    {
+          console.log("working daily");
+          
+    }
+}
+
+const alien1 = new Alien("sai", 'html');
+const alien2 = new Alien('kiran', 'css');
+
+console.log(alien1);
+alien2.work();
+
