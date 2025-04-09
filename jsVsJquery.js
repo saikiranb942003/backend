fd8e8f4833cdb4c4c05165b69076a8239cebe71e
a_rihant <arijain@gammastack.com>
2025-04-09 16:40:14 +0530

    second commit

--- jsVsJquery.js
+++ jsVsJquery.js
@@ -33,6 +33,43 @@ content = myElement.html();
 content = myElement.innerHTML;
 
 
+//javascript :
+
+-> it is the core language of the web
+-> gives us full control over browser behavior, DOM manipulatin, events etc
+-> more powerful and flexiable 
+-> we can use it in the browser (no extra libraries are needed)
+-> it is faster in performance
+-> it is a language
+-> it does not require any dependencies
+-> older js code needed lot of compatibility checks
+-> uses fetch() or XMLHttpRequest 
+-> we call methods seperatly unless we build our own chaining logic
+
+
+// Jquery
+
+-> it is a library in the javascrit
+-> it is used to simpify common js tasks like DOM manipulation, event handling
+animations, Ajax etc
+-> in js we have to write more code to do the same thing
+but using this it can be done in a easy way
+->we write less syntax that is less code to write
+-> it is slower compared to js
+-> it is a library
+-> it requires jQuery library
+-> it is not so popular now
+-> it was created specially for browser differences - works consistently 
+across all browsers
+-> has buid-in $.ajax(), $.get(), $.post() which are simpler to use
+-> supports method chaining by default
+
+
+// eg :
+$("#myDiv").hide();
+
+
+
 
 */
 
