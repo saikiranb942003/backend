8e56054620a2875f56df22bd5dce4dc4a0606ffd
a_rihant <arijain@gammastack.com>
2025-04-10 18:12:40 +0530

    first commit

---
+++ eventBubbling.js
@@ -0,0 +1,17 @@
+document.querySelector("#grandparent")
+.addEventListener('click', () => {
+    console.log("grandparent clicked");
+    
+});
+
+document.querySelector("#parent")
+.addEventListener('click', () => {
+    console.log("Parent clicked");
+    
+});
+
+document.querySelector("#child")
+.addEventListener('click', () => {
+    console.log("child clicked");
+    
+});
