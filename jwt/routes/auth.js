941db4a2494aca23ab3a6cd82fcecb848a6b6f8c
a_rihant <arijain@gammastack.com>
2025-05-15 17:48:45 +0530

    second commit

--- jwt/routes/auth.js
+++ jwt/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const passport = require('passport');
+//const passport = require('passport');
 const { register, login } = require('../controllers/authController');
 const router = express.Router();
 
